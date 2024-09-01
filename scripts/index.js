7be95322ba41519b597052d1f5d43560df5a4a6a
s.sotnikov <sotnseriy@yandex.ru>
2024-09-01 20:52:40 +0300

    initial commit index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,17 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const template = document.querySelector('#card-template');
+const cardList = document.querySelector('.places__list');
+const addCardButton = document.querySelector('.profile__add-button');
+const deleteCardButton = document.querySelector('.card__delete-button');
+
+const addCard = function(){
+
+}
+
+const removeCard = function() {
+    
+}
