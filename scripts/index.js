5116cb4fca64c9253213421b9f261d6f6717ba37
s.sotnikov <sotnseriy@yandex.ru>
2024-09-01 22:32:45 +0300

    adding card and removing spesial card

--- scripts/index.js
+++ scripts/index.js
@@ -9,15 +9,23 @@
 // @todo: Вывести карточки на страницу
 
 
-const template = document.querySelector('#card-template');
+const template = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 const addCardButton = document.querySelector('.profile__add-button');
 const deleteCardButton = document.querySelector('.card__delete-button');
 
-const addCard = function(){
+const createCard = (name, link, deleteCallback) => {
+    const cardElement = template.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
 
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback)
+
+    return cardElement;
 }
 
-const removeCard = function() {
-    
-}
+const removeCard = e => e.target.closest('.places__item').remove();
+
+const addCards = () => initialCards.forEach(elem => cardList.append(createCard(elem.name, elem.link, removeCard)));
+
+addCards();
