fbc3fc6507e46feb3e9adddb02a5066d1144e632
s.sotnikov <sotnseriy@yandex.ru>
2024-09-02 10:25:53 +0300

    refactor deleteCard and remove unused const

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,19 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-
 const template = document.querySelector('#card-template').content;
-const cardList = document.querySelector('.places__list');
-const addCardButton = document.querySelector('.profile__add-button');
-const deleteCardButton = document.querySelector('.card__delete-button');
+const cardsContainer = document.querySelector('.places__list');
 
 const createCard = (name, link, deleteCallback) => {
     const cardElement = template.querySelector('.places__item').cloneNode(true);
+    
     cardElement.querySelector('.card__title').textContent = name;
     cardElement.querySelector('.card__image').src = link;
-
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback)
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCallback(cardElement));
 
     return cardElement;
 }
 
-const removeCard = e => e.target.closest('.places__item').remove();
+const removeCard = card => card.remove();
 
-const addCards = () => initialCards.forEach(elem => cardList.append(createCard(elem.name, elem.link, removeCard)));
+const addCards = () => initialCards.forEach(elem => cardsContainer.append(createCard(elem.name, elem.link, removeCard)));
 
 addCards();
